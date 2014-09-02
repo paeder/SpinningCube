7a018654ca94bcf09e48451c9ab16e3f316c5023
Patrick Aeder <paeder@mobiquityinc.com>
2014-09-02 11:39:24 -0400

    initial commit

---
+++ Assets/scripts/spinScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spinScript : MonoBehaviour {
+
+	public GUIText cubeText;
+
+	public int speed;
+	public int spinDuration;
+	private long startTime;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		startTime = (long) Time.time;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.Rotate(Vector3.up, speed * Time.deltaTime);
+
+		long duration = (long) Time.time - startTime;
+		checkForExit (duration);
+
+	}
+
+	void checkForExit(long duration){
+
+		if (duration > spinDuration) {
+	
+			// exit the application
+			cubeText.text = "App Exited";
+			Application.Quit ();
+		} else {
+					
+			long remaingingTime = spinDuration - duration;			
+			cubeText.text = "App will close in: "+remaingingTime;
+		}
+	}	
+}
